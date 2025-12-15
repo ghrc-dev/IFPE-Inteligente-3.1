7e18587e4e2cbd10cb7773ae83d59a89590ce404
ghrc-dev <ghrc@discente.ifpe.edu.br>
2025-12-15 16:33:20 -0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,9 +1,18 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
 
-cred = credentials.Certificate("firebase_key.json")
+# Lê o JSON do Firebase pelas variáveis de ambiente
+firebase_json = json.loads(os.environ["FIREBASE_CREDENTIALS"])
 
+# Corrige quebra de linha da private_key
+firebase_json["private_key"] = firebase_json["private_key"].replace("\\n", "\n")
+
+# Inicializa o Firebase UMA ÚNICA VEZ
 if not firebase_admin._apps:
+    cred = credentials.Certificate(firebase_json)
     firebase_admin.initialize_app(cred)
 
+# Exporta serviços
 db = firestore.client()
